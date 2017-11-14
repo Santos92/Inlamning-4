f1a7b91045284a9d1791fe2dde470dcdbd82ea63
Akinyi <margaretaoch@gmail.com>
2017-11-14 16:51:04 +0100

    Början på ServerSessionHandler

--- Inlamning4/src/ServerSide/SessionHandling/ServerSessionHandler.java
+++ Inlamning4/src/ServerSide/SessionHandling/ServerSessionHandler.java
@@ -1,5 +1,27 @@
 package ServerSide.SessionHandling;
 
+import java.lang.Thread.State;
+
 public class ServerSessionHandler {
+	
+
+String question;
+String answer;
+boolean verdict;
+static final int WAITING =0;
+static final int QUESTION =1;
+static final int ANSWER = 2;
+int pointCounter =0;
+
+public ServerSessionHandler(String session) { // konstruktor
+	question = r;
+	verdict = null;
+	
+	
+	
+	
+}
+
+
 
 }
