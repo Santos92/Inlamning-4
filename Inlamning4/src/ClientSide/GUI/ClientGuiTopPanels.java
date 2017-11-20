c7a3e19485fb0893dcb6ea978d0ad021e3a21089
amandaiwan <amanda@iwanowski.com>
2017-11-20 13:47:02 +0100

    testing

---
+++ Inlamning4/src/ClientSide/GUI/ClientGuiTopPanels.java
@@ -0,0 +1,102 @@
+package ClientSide.GUI;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.net.URL;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ClientGuiTopPanels {
+
+	private ClientGUI GUI;
+	private ColorThemes Tema;
+	private Font headingFont;
+	
+	private JButton refresh = new JButton("Refresh");
+	private JButton statistik = new JButton("Statistik");
+	private JButton inställningar = new JButton("Inställningar");
+	
+	public ClientGuiTopPanels(ColorThemes Tema)
+	{
+		this.Tema = Tema;
+	}
+	
+	public JPanel TopwithoutButtons() {
+		JPanel top = new JPanel();
+		top.setLayout(new BoxLayout(top, BoxLayout.LINE_AXIS));
+		top.setBackground(Tema.getTopBG());
+		headingFont = new Font("Arial", Font.BOLD, 40);
+		top.setPreferredSize(new Dimension(500, 60));
+		JLabel titel = new JLabel(" Quizkampen");
+		titel.setForeground(Tema.getText());
+		titel.setFont(headingFont);
+		
+		top.add(titel);
+		return top;
+	}
+	
+	public JPanel Top() {
+		refresh.removeActionListener(GUI);
+		statistik.removeActionListener(GUI);
+		inställningar.removeActionListener(GUI);
+	
+		JPanel top = new JPanel();
+		top.setLayout(new BoxLayout(top, BoxLayout.LINE_AXIS));
+		top.setBackground(Tema.getTopBG());
+		headingFont = new Font("Arial", Font.BOLD, 33);
+		top.setPreferredSize(new Dimension(500, 50));
+		JLabel titel = new JLabel(" Quizkampen");
+		titel.setForeground(Tema.getText());
+		titel.setFont(headingFont);
+		
+		JPanel btnHolder = new JPanel();
+		btnHolder.setLayout(new GridLayout(0,3));
+		
+		refresh = new JButton("Refresh");
+		refresh.setBorderPainted(false);
+		refresh.setBackground(Tema.getTopBG());
+		refresh.setForeground(Tema.getText());
+		
+		statistik = new JButton("Statistik");
+		statistik.setBorderPainted(false);
+		statistik.setBackground(Tema.getTopBG());
+		statistik.setForeground(Tema.getText());
+		
+		inställningar = new JButton("Inställningar");
+		inställningar.setBorderPainted(false);
+		inställningar.setBackground(Tema.getTopBG());
+		inställningar.setForeground(Tema.getText());
+		
+		refresh.addActionListener(GUI);
+		statistik.addActionListener(GUI);
+		inställningar.addActionListener(GUI);
+		
+		btnHolder.add(refresh);
+		btnHolder.add(statistik);
+		btnHolder.add(inställningar);
+		
+
+		top.add(titel);
+		top.add(btnHolder);
+		
+		return top;
+	}
+	
+	public JButton getRefreshBtn() {
+		return refresh;
+	}
+	public JButton getStatistikBtn() {
+		return statistik;
+	}
+	public JButton getSettingBtn() {
+		return inställningar;
+	}
+
+	
+	
+}
---
+++ Inlamning4/src/ServerSide/Users/UserDatabase.java
@@ -0,0 +1,101 @@
+package ServerSide.Users;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import Communication.Session;
+import Communication.Session.gameStates;
+import ServerSide.ClientThread;
+
+public class UserDatabase {
+	
+	private static ArrayList<User> allUsers = new ArrayList<>();
+	private File UserDatabase = new File("Inlamning4\\src\\ServerSide\\Users\\AllUsers.txt");
+	
+	public UserDatabase()
+	{
+		UserDatabase = new File("Inlamning4\\src\\ServerSide\\Users\\AllUsers.txt");
+		if(!UserDatabase.exists())
+			UserDatabase = new File("src\\ServerSide\\Users\\AllUsers.txt");
+	}
+	
+	public void register(String name, String pass, ClientThread server) {
+		boolean userExist = false;
+		for(User x : allUsers)
+		{
+			if(x.getNamn().equals(name))
+			{
+				Session sess = new Session();
+				sess.setState(gameStates.FailReg);
+				sess.setMessage("Användare finns redan med namnet! försök igen.");
+				sess.setUserName(name);
+				server.send(sess);
+				userExist = true;
+				break;
+			}
+		}
+		if(!userExist){
+			allUsers.add(new User(name, pass));
+			Session sess = new Session();
+			sess.setState(gameStates.OkReg);
+			sess.setMessage("Successful registration");
+			server.send(sess);
+		}
+	}
+	public void sparaLista()
+	{
+		try {
+	 FileOutputStream fos = new FileOutputStream(UserDatabase);
+	 ObjectOutputStream oos = new ObjectOutputStream(fos);
+	 oos.writeObject(allUsers);
+	 oos.close();
+	}catch(Exception e) { 
+		 e.printStackTrace();
+	}
+}
+	public void laddaLista()
+	{
+	    try {
+			FileInputStream fis = new FileInputStream(UserDatabase);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			allUsers = (ArrayList<User>) ois.readObject();
+			
+			ois.close();
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public void login(String name, String pass, ClientThread server) {
+		
+		boolean loggedIn = false;
+		for(User x : allUsers)
+			
+		{
+			if(x.getNamn().equalsIgnoreCase(name) && x.getPassword().equalsIgnoreCase(pass)) 
+			{
+				Session sess = new Session();
+				sess.setState(gameStates.AuthAccept);
+				sess.setMessage("Welcome");
+				sess.setUserName(name);
+				server.send(sess);
+				loggedIn = true;
+				break;
+				
+			}
+		}
+		if(!loggedIn) {
+			Session sess = new Session();
+			sess.setState(gameStates.FailAuth);
+			sess.setMessage("Wrong credentials");
+			server.send(sess);
+			
+		}
+	}
+}
